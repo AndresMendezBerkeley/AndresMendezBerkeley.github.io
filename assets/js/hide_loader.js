e4f4e087a4cb62d5c6215d54d45f781bcc82e203
Andres Guillermo Mendez <agmendez@berkeley.edu>
2021-01-07 15:51:57 -0800

    editted loader page so that it is fixed to viewport and not the top of the page. also made it so that we pull in the videos after the page has loaded

--- assets/js/hide_loader.js
+++ assets/js/hide_loader.js
@@ -1,8 +1,27 @@
 // hide the scroll bar while loading
 document.body.style.overflowY = 'hidden';
 
+
 // once all elements load, hide the loader animation and show the scrollbar
 onload = function loaderAnim() {
     document.querySelector(".loader-container").classList.add("loader-fade-out");
     document.body.style.overflowY = 'visible';
+
+    // play the robot animation on the home page.
+    // start playing other animations on the webpage
+    if (document.querySelector(".robot")) {
+        var frames = document.querySelector(".robot").children;
+        var frameCount = frames.length;
+        var i = 0;
+        setInterval(function () { 
+            frames[i % frameCount].style.display = "none";
+            frames[++i % frameCount].style.display = "block";
+        }, 100);
+    }
+
+    // add the videos into their place.
+    if (document.querySelector("#first_step_vid")) {
+        document.querySelector("#first_step_vid").setAttribute('src', 'assets/img/first_step.mp4');
+        document.querySelector("#another_sim_vid").setAttribute('src', 'assets/img/second_vid_thin.mp4');   
+    }
 }
